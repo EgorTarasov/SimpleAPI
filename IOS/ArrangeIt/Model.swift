2a62717c6d6fb9ec865125b97be37bbe72ac75c8
Тарасов Егор <tarasov.egor.yandex@gmail.com>
2020-07-16 11:01:45 +0300

    +Fixed IOS version and model.swift +Added User and Event SetUp in ViewController.swft

--- IOS/ArrangeIt/Model.swift
+++ IOS/ArrangeIt/Model.swift
@@ -43,4 +43,4 @@ struct UserStorage {
     
 }
 
-User(id: "-1", name: "Test User", image: nil, isAppUser: true, administratedEvents: [Event(id: "1", name: "TestingEvent1", happeningDate: Date(), place: (56.7, 40.1), creatingDate: Date(), owner: "-1", description: "This is descritpion for 1 test event", image: nil, willGoUsers: ["-1", "2"], mayGoUsers: [])], nonadministritedEvents: [Event(id: "2", name: "TestingEvent2", happeningDate: Date(), place: (50.3, 43.5), creatingDate: Date(), owner: "2", description: "This is descritpion for 2 test event", image: nil, willGoUsers: [2], mayGoUsers: [])])
+
